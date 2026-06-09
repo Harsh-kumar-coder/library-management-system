93fbfb58fd6ee27224ca29cb67c5b00a941fd9a4
Harsh-kumar-coder <kumarharsh09374@gmail.com>
2026-06-09 21:45:25 +0530

    Update project

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,61 +2,23 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from .models import Profile
 
-
 class CustomUserAdmin(admin.ModelAdmin):
+    list_display=('username','email','get_id_value','is_active','is_staff','is_superuser')
+    list_filter=('is_active','is_staff','is_superuser')
+    search_fields=('username','email','first_name')
+    ordering=('username',)
 
-    list_display = (
-        'username',
-        'email',
-        'get_id_value',
-        'is_active',
-        'is_staff',
-        'is_superuser',
-    )
-
-    list_filter = (
-        'is_active',
-        'is_staff',
-        'is_superuser',
-    )
-
-    search_fields = (
-        'username',
-        'email',
-        'first_name',
-    )
-
-    ordering = ('username',)
-
-    def get_id_value(self, obj):
-        return obj.first_name
-
-    get_id_value.short_description = "Roll No / Staff ID"
-
+    def get_id_value(self,obj): return obj.first_name
+    get_id_value.short_description="Roll No / Staff ID"
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    list_display=('user','role','user_id_value')
+    list_filter=('role',)
+    search_fields=('user__username','user__email')
 
-    list_display = (
-        'user',
-        'role',
-        'user_id_value',
-    )
-
-    list_filter = (
-        'role',
-    )
-
-    search_fields = (
-        'user__username',
-        'user__email',
-    )
-
-    def user_id_value(self, obj):
-        return obj.user.first_name
-
-    user_id_value.short_description = "Roll No / Staff ID"
-
+    def user_id_value(self,obj): return obj.user.first_name
+    user_id_value.short_description="Roll No / Staff ID"
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
--- books/admin.py
+++ books/admin.py
@@ -1,118 +1,32 @@
 from django.contrib import admin
-from .models import (
-    Book,
-    IssuedBook,
-    Donation,
-    BookRequest,
-    ContactMessage
-)
-
+from .models import Book, IssuedBook, Donation, BookRequest, ContactMessage
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-
-    list_display = (
-        'id',
-        'title',
-        'author',
-        'isbn',
-        'quantity',
-        'added_at',
-    )
-
-    list_filter = (
-        'added_at',
-    )
-
-    search_fields = (
-        'title',
-        'author',
-        'isbn',
-    )
-
+    list_display=('id','title','author','isbn','quantity','added_at')
+    list_filter=('added_at',)
+    search_fields=('title','author','isbn')
 
 @admin.register(IssuedBook)
 class IssuedBookAdmin(admin.ModelAdmin):
-
-    list_display = (
-        'id',
-        'student',
-        'book',
-        'issue_date',
-        'return_date',
-        'fine',
-        'returned',
-    )
-
-    list_filter = (
-        'returned',
-        'issue_date',
-    )
-
-    search_fields = (
-        'student__username',
-        'book__title',
-    )
-
+    list_display=('id','student','book','issue_date','return_date','fine','returned')
+    list_filter=('returned','issue_date')
+    search_fields=('student__username','book__title')
 
 @admin.register(BookRequest)
 class BookRequestAdmin(admin.ModelAdmin):
-
-    list_display = (
-        'id',
-        'student',
-        'book',
-        'requested_at',
-        'approved',
-    )
-
-    list_filter = (
-        'approved',
-        'requested_at',
-    )
-
-    search_fields = (
-        'student__username',
-        'book__title',
-    )
-
+    list_display=('id','student','book','requested_at','approved')
+    list_filter=('approved','requested_at')
+    search_fields=('student__username','book__title')
 
 @admin.register(Donation)
 class DonationAdmin(admin.ModelAdmin):
-
-    list_display = (
-        'id',
-        'name',
-        'amount',
-        'created_at',
-    )
-
-    list_filter = (
-        'created_at',
-    )
-
-    search_fields = (
-        'name',
-        'message',
-    )
+    list_display=('id','name','amount','created_at')
+    list_filter=('created_at',)
+    search_fields=('name','message')
 
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
-
-    list_display = (
-        'id',
-        'name',
-        'email',
-        'subject',
-        'created_at',
-    )
-
-    search_fields = (
-        'name',
-        'email',
-        'subject',
-    )
-
-    list_filter = (
-        'created_at',
-    )
+    list_display=('id','name','email','subject','created_at')
+    list_filter=('created_at',)
+    search_fields=('name','email','subject')
